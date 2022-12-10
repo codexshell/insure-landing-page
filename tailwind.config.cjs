0f3ef204ef16f741b2a63785738f1f6c626f599d
Silvanos Eric <52836290+codexshell@users.noreply.github.com>
2022-12-10 13:13:26 +0300

    extend tailwind with design tokens

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "p-dark-violet": "hsl(256, 26%, 20%)",
+        "p-grayish-blue": "hsl(216, 30%, 68%)",
+        "n-very-dark-violet": "hsl(270, 9%, 17%)",
+        "n-dark-grayish-violet": "hsl(273, 4%, 51%)",
+        "n-very-light-gray": "hsl(0, 0%, 98%)",
+      },
+      fontSize: {
+        "body-copy": "16px",
+      },
+      fontFamily: {
+        headings: ["DM Serif Display", "serif"],
+        body: ["Karla", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
