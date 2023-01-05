645ad7f73e5b67d27907f6ef935d8b87d630899e
Silvanos Eric <52836290+codexshell@users.noreply.github.com>
2023-01-05 03:08:35 +0300

    feat: add hero section

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,9 +1,15 @@
 import MainNav from "../MainNav";
+import Hero from "../Hero";
 
 function App() {
   return (
     <div className="font-body text-base">
-      <MainNav />
+      <header>
+        <MainNav />
+        <Hero />
+      </header>
+      <main></main>
+      <footer></footer>
     </div>
   );
 }
---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,24 @@
+import mobileImageIntro from "../../assets/image-intro-mobile.jpg";
+import Button from "../Button";
+
+export default function () {
+  return (
+    <div className="bg-p-dark-violet">
+      <div>
+        <img src={mobileImageIntro} alt="an image of a family walking" />
+      </div>
+
+      <div className="px-5 py-28 text-n-very-light-gray text-center bg-mobile-intro-left-pattern bg-no-repeat">
+        <h1 className="font-header text-5xl">Humanitizing your insurance.</h1>
+        <p className="mt-6 font-normal mb-6">
+          Get your life insurance coverage easier and faster. We blend our
+          expertise and technology to help you find the plan that's right for
+          you. Ensure you and your loved ones are protected.
+        </p>
+        <a href="#">
+          <Button>View Plans</Button>
+        </a>
+      </div>
+    </div>
+  );
+}
