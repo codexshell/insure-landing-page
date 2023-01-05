cfcfb953b7004bd359b84c2248fa6e03d6fd57ad
Silvanos Eric <52836290+codexshell@users.noreply.github.com>
2023-01-06 00:27:20 +0300

    add active state to button component

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,6 +1,6 @@
 export default function Button({ children }) {
   return (
-    <button className="uppercase border-2 py-2 px-8 active:bg-n-very-light-gray active:text-n-very-dark-violet">
+    <button className="uppercase border-2 py-2 px-8 active:bg-n-very-light-gray active:text-p-dark-violet">
       {children}
     </button>
   );
