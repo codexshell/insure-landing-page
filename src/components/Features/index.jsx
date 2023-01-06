4be58cf704bee85b6bf13fb46d443f4ec6c6b0c1
Silvanos Eric <52836290+codexshell@users.noreply.github.com>
2023-01-06 23:23:41 +0300

    chore: create a Features component
    this component will aggregate several Feature components

---
+++ src/components/Features/index.jsx
@@ -0,0 +1,18 @@
+import Feature from "../Feature";
+import { features } from "./data";
+
+export default function Features() {
+  return (
+    <section className="text-center mt-32 px-[18px]">
+      <hr className="border-t border-p-dark-violet bg-p-dark-violet w-44 mx-auto" />
+
+      <h1 className="font-header text-5xl mt-10">We're different</h1>
+
+      <div className="mt-20">
+        {features.map((feature) => (
+          <Feature feature={feature} />
+        ))}
+      </div>
+    </section>
+  );
+}
