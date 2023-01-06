5bebb29b98d68972d5ff797d8457fbc2509dad42
Silvanos Eric <52836290+codexshell@users.noreply.github.com>
2023-01-06 23:24:43 +0300

    chore: set up data used by Features component
    data.js will contain a list of features that can be dynamically rendered
    by the Features component

---
+++ src/components/Features/data.js
@@ -0,0 +1,24 @@
+import snappyImgUrl from "../../assets/icon-snappy-process.svg";
+import affordableImgUrl from "../../assets/icon-affordable-prices.svg";
+import peopleImgUrl from "../../assets/icon-people-first.svg";
+
+export const features = [
+  {
+    name: "Snappy Process",
+    description:
+      "Our application process can be completed in minutes, not hours. Don't get stuck filling in tedious forms.",
+    img: snappyImgUrl,
+  },
+  {
+    name: "Affordable Prices",
+    description:
+      "We don't want you worrying about hight monthly costs. Our prices may be low, but we still offer the best coverage possible",
+    img: affordableImgUrl,
+  },
+  {
+    name: "People First",
+    description:
+      "Our plans aren't full of conditions and clauses to prevent payouts. We make sure you're covered when you need it.",
+    img: peopleImgUrl,
+  },
+];
