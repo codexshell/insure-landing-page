59e0122201af429294736873b04271e8e420cbae
Silvanos Eric <52836290+codexshell@users.noreply.github.com>
2023-01-06 23:22:47 +0300

    chore: create a Feature component

---
+++ src/components/Feature/index.jsx
@@ -0,0 +1,13 @@
+export default function Feature({ feature }) {
+  return (
+    <div className="[&:not(:first-child)]:mt-14">
+      <div className="inline-block">
+        <img src={feature.img} alt="" />
+      </div>
+
+      <h3 className="font-header text-3xl mt-6">{feature.name}</h3>
+
+      <p className="mt-5 text-n-dark-grayish-violet">{feature.description}</p>
+    </div>
+  );
+}
