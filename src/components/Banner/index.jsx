39ee4a2977846e8420792e29166866af1e2b6b72
Silvanos Eric <52836290+codexshell@users.noreply.github.com>
2023-01-07 00:26:14 +0300

    chore: add background image to Banner component

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -2,7 +2,7 @@ import Button from "../Button";
 
 export default function Banner() {
   return (
-    <div className="bg-p-dark-violet text-center mx-6 px-4 py-12 text-n-very-light-gray">
+    <div className="bg-p-dark-violet bg-mobile-how-we-work-bg-pattern bg-no-repeat bg-right text-center mx-6 px-4 py-12 text-n-very-light-gray">
       <h3 className="font-header text-4xl mb-8">
         Find out more about how we work
       </h3>
