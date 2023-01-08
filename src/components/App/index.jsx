7fcbbb60c8280e33506b29ab8a42db77aff94103
Silvanos Eric <52836290+codexshell@users.noreply.github.com>
2023-01-08 23:00:55 +0300

    chore: add decorative pattern

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -3,6 +3,7 @@ import Hero from "../Hero";
 import Features from "../Features";
 import Banner from "../Banner";
 import Footer from "../Footer";
+import mobileIntroRightPatternUrl from "../../assets/bg-pattern-intro-right-mobile.svg";
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
         <MainNav />
       </header>
       <main className="pb-36">
+        <div className="absolute right-0 top-[110%]">
+          <img src={mobileIntroRightPatternUrl} alt="" />
+        </div>
         <Hero />
         <Features />
         <Banner />
