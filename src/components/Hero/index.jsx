5eb516431aa33a4cde78221aa9957c576529bbee
Silvanos Eric <52836290+codexshell@users.noreply.github.com>
2023-01-08 23:30:23 +0300

    chore: update background pattern for `sm` devices

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,14 +1,23 @@
-import mobileImageIntro from "../../assets/image-intro-mobile.jpg";
+import mobileIntroImage from "../../assets/image-intro-mobile.jpg";
 import Button from "../Button";
+import desktopIntroLeftBgPattern from "../../assets/bg-pattern-intro-right-desktop.svg";
+import desktopIntroImage from "../../assets/image-intro-desktop.jpg";
 
 export default function () {
   return (
-    <div className="bg-p-dark-violet">
+    <div className="bg-p-dark-violet relative">
+      <div className="absolute top-0 right-0">
+        <img src={desktopIntroLeftBgPattern} alt="" />
+      </div>
       <div>
-        <img className="mx-auto" src={mobileImageIntro} alt="an image of a family walking" />
+        <img
+          className="mx-auto"
+          src={mobileIntroImage}
+          alt="an image of a family walking"
+        />
       </div>
 
-      <div className="px-5 py-28 text-n-very-light-gray text-center bg-mobile-intro-left-pattern bg-no-repeat">
+      <div className="px-5 py-28 text-n-very-light-gray text-center bg-mobile-intro-left-pattern bg-no-repeat sm:bg-none">
         <h1 className="font-header text-5xl">Humanitizing your insurance.</h1>
         <p className="mt-6 font-normal mb-8">
           Get your life insurance coverage easier and faster. We blend our
