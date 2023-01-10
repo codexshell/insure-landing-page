19fe528e7c7144bf5c30da8f68fb439425972921
Silvanos Eric <52836290+codexshell@users.noreply.github.com>
2023-01-11 01:59:05 +0300

    fix: switch from managing the open and close state of the menu using state
    an attemtp to use state to manage the opening and closing of the
    navigation drawer led to a subtle bug.
    when the website is opened on desktop and resized to a mobile screen,
    the navigation menu doesn't automatically go away.
    it required the use to manually click the hamburger icon to hide it.
    a more lightweight and concise solution was achieved using pure css

--- src/components/MainNav/index.jsx
+++ src/components/MainNav/index.jsx
@@ -6,24 +6,15 @@ import logoUrl from "../../assets/logo.svg";
 const navItems = ["how we work", "blog", "account", "view plans"];
 
 export default function MainNav() {
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-  const [isXl, setIsXl] = useState(false);
+  const handleClick = () => {
+    const openIcon = document.querySelector("#openIcon");
+    const closeIcon = document.querySelector("#closeIcon");
+    const drawer = document.querySelector("#drawer");
 
-  const resizeObserver = new ResizeObserver((entries) => {
-    // when the size of the body is greater than 1280px show main menu
-    for (const entry of entries) {
-      const borderBoxSize = entry.borderBoxSize[0];
-      const inlineSize = borderBoxSize.inlineSize;
-      if (inlineSize >= 1280) {
-        setIsXl(true);
-        setIsDrawerOpen(true);
-      } else {
-        setIsXl(false);
-      }
-    }
-  });
-
-  resizeObserver.observe(document.body);
+    openIcon.classList.toggle("hidden");
+    closeIcon.classList.toggle("hidden");
+    drawer.classList.toggle("hidden");
+  };
 
   return (
     <nav>
@@ -32,36 +23,36 @@ export default function MainNav() {
           <img src={logoUrl} alt="Insure logo" />
         </a>
 
-        <div
-          className="cursor-pointer xl:hidden"
-          onClick={() => setIsDrawerOpen(!isDrawerOpen)}
-        >
-          {isDrawerOpen ? (
-            <img src={iconCloseUrl} alt="icon close image" />
-          ) : (
-            <img src={iconsOpenUrl} alt="icon open image" />
-          )}
+        <div onClick={handleClick} className="cursor-pointer xl:hidden">
+          <img
+            id="closeIcon"
+            src={iconCloseUrl}
+            alt="icon close image"
+            className="hidden"
+          />
+          <img id="openIcon" src={iconsOpenUrl} alt="icon open image" />
         </div>
       </div>
 
-      {isDrawerOpen && (
-        <div className="bg-n-very-dark-violet bg-mobile-nav-pattern bg-no-repeat bg-bottom px-16">
-          <ul className="flex flex-col items-center text-n-very-light-gray py-10 px-4 gap-2 min-h-[75vh] md:min-h-[65vh] lg:min-h-[50vh]">
-            {navItems.map((item) => (
-              <li className="uppercase text-xl w-full text-center" key={item}>
-                <a
-                  className={`w-full h-full inline-block py-4 hover:bg-n-very-light-gray hover:text-n-very-dark-violet active:bg-n-very-dark-violet active:text-n-very-light-gray ${
-                    item.toLowerCase() === "view plans" && "border-2"
-                  }`}
-                  href="#"
-                >
-                  {item}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
+      <div
+        id="drawer"
+        className="bg-n-very-dark-violet bg-mobile-nav-pattern bg-no-repeat bg-bottom px-16 hidden xl:block"
+      >
+        <ul className="flex flex-col items-center text-n-very-light-gray py-10 px-4 gap-2 min-h-[75vh] md:min-h-[65vh] lg:min-h-[50vh]">
+          {navItems.map((item) => (
+            <li className="uppercase text-xl w-full text-center" key={item}>
+              <a
+                className={`w-full h-full inline-block py-4 hover:bg-n-very-light-gray hover:text-n-very-dark-violet active:bg-n-very-dark-violet active:text-n-very-light-gray ${
+                  item.toLowerCase() === "view plans" && "border-2"
+                }`}
+                href="#"
+              >
+                {item}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </nav>
   );
 }
