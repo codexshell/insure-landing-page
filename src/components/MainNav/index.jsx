21bcba2b97d769d79144ea9b85493c1d6ae17ede
Silvanos Eric <52836290+codexshell@users.noreply.github.com>
2023-01-11 01:27:58 +0300

    chore: show navigation menu on lg screens by default

--- src/components/MainNav/index.jsx
+++ src/components/MainNav/index.jsx
@@ -6,17 +6,37 @@ import logoUrl from "../../assets/logo.svg";
 const navItems = ["how we work", "blog", "account", "view plans"];
 
 export default function MainNav() {
-  const [drawerOpen, setIsOpen] = useState(false); 
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [isXl, setIsXl] = useState(false);
+
+  const resizeObserver = new ResizeObserver((entries) => {
+    // when the size of the body is greater than 1280px show main menu
+    for (const entry of entries) {
+      const borderBoxSize = entry.borderBoxSize[0];
+      const inlineSize = borderBoxSize.inlineSize;
+      if (inlineSize >= 1280) {
+        setIsXl(true);
+        setIsDrawerOpen(true);
+      } else {
+        setIsXl(false);
+      }
+    }
+  });
+
+  resizeObserver.observe(document.body);
 
   return (
     <nav>
-      <div className="flex justify-between p-6 items-center">
+      <div className="flex justify-between p-6 xl:p-8 items-center">
         <a href="#">
           <img src={logoUrl} alt="Insure logo" />
         </a>
 
-        <div className="cursor-pointer" onClick={() => setIsOpen(!drawerOpen)}>
-          {drawerOpen ? (
+        <div
+          className="cursor-pointer xl:hidden"
+          onClick={() => setIsDrawerOpen(!isDrawerOpen)}
+        >
+          {isDrawerOpen ? (
             <img src={iconCloseUrl} alt="icon close image" />
           ) : (
             <img src={iconsOpenUrl} alt="icon open image" />
@@ -24,11 +44,11 @@ export default function MainNav() {
         </div>
       </div>
 
-      {drawerOpen && (
+      {isDrawerOpen && (
         <div className="bg-n-very-dark-violet bg-mobile-nav-pattern bg-no-repeat bg-bottom px-16">
           <ul className="flex flex-col items-center text-n-very-light-gray py-10 px-4 gap-2 min-h-[75vh] md:min-h-[65vh] lg:min-h-[50vh]">
             {navItems.map((item) => (
-              <li className="uppercase text-xl w-full text-center">
+              <li className="uppercase text-xl w-full text-center" key={item}>
                 <a
                   className={`w-full h-full inline-block py-4 hover:bg-n-very-light-gray hover:text-n-very-dark-violet active:bg-n-very-dark-violet active:text-n-very-light-gray ${
                     item.toLowerCase() === "view plans" && "border-2"
