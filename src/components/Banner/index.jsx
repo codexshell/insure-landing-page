4111833cc68a102db16de880a9f1971aacf9507c
Silvanos Eric <52836290+codexshell@users.noreply.github.com>
2023-01-11 19:06:39 +0300

    chore: make banner responsive to 2xl screens

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -2,9 +2,10 @@ import Button from "../Button";
 
 export default function Banner() {
   return (
-    <div className="xl:flex justify-around items-center bg-p-dark-violet bg-mobile-how-we-work-bg-pattern xl:bg-desktop-how-we-work-bg-pattern bg-no-repeat bg-right-top text-center xl:text-start mx-6 px-4 py-20 xl:py-16 text-n-very-light-gray">
-      <h3 className="font-header text-4xl xl:text-6xl mb-8 xl:m-0 basis-1/2 xl:px-6">
-        Find out more about how we work
+    <div className="xl:flex justify-between items-center bg-p-dark-violet bg-mobile-how-we-work-bg-pattern xl:bg-desktop-how-we-work-bg-pattern bg-no-repeat bg-right-top text-center xl:text-start mx-6 px-8 xl:px-20 py-20 xl:py-16 text-n-very-light-gray">
+      <h3 className="font-header text-4xl xl:text-5xl mb-8 xl:m-0 xl:basis-[55%]">
+        Find out more
+        <br /> about how we work
       </h3>
 
       <div>
