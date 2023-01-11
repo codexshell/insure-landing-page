57fb23fd6aea8aa4bc59a16165621a51d30ba084
Silvanos Eric <52836290+codexshell@users.noreply.github.com>
2023-01-11 17:59:53 +0300

    chore: make hero section responsive to xl screens

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -4,6 +4,7 @@ import Features from "../Features";
 import Banner from "../Banner";
 import Footer from "../Footer";
 import mobileIntroRightPatternUrl from "../../assets/bg-pattern-intro-right-mobile.svg";
+import introLeftBgPattern from "../../assets/bg-pattern-intro-left-desktop.svg";
 
 function App() {
   return (
@@ -13,6 +14,9 @@ function App() {
       </header>
       <main className="pb-36 relative">
         <Hero />
+        <div className="hidden xl:block absolute top-[500px]">
+          <img src={introLeftBgPattern} alt="" />
+        </div>
         <div className="sm:px-36 md:px-48 lg:px-80">
           <Features />
           <Banner />
--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,23 +1,28 @@
 import mobileIntroImage from "../../assets/image-intro-mobile.jpg";
 import Button from "../Button";
-import desktopIntroLeftBgPattern from "../../assets/bg-pattern-intro-right-desktop.svg";
+import desktopIntroRightBgPattern from "../../assets/bg-pattern-intro-right-desktop.svg";
+import desktopIntroImage from "../../assets/image-intro-desktop.jpg";
 
-export default function Hero () {
+export default function Hero() {
   return (
-    <div className="bg-p-dark-violet relative">
+    <div className="xl:py-32 xl:px-16 bg-p-dark-violet relative xl:flex">
       <div className="absolute top-0 right-0 hidden sm:block">
-        <img src={desktopIntroLeftBgPattern} alt="" />
+        <img src={desktopIntroRightBgPattern} alt="" />
       </div>
-      <div>
+      <div className="xl:basis-1/2 order-1 relative">
         <img
-          className="mx-auto"
+          className="mx-auto xl:absolute right-0"
+          srcSet={`${mobileIntroImage} 375w, ${desktopIntroImage} 540w`}
+          sizes="(max-width: 600px) 480px,
+                  540px"
           src={mobileIntroImage}
           alt="an image of a family walking"
         />
       </div>
 
-      <div className="px-5 sm:px-32 md:px-48 lg:px-80 py-28 text-n-very-light-gray text-center bg-mobile-intro-left-pattern bg-no-repeat sm:bg-none">
-        <h1 className="font-header text-5xl">Humanitizing your insurance.</h1>
+      <div className="xl:flex flex-col xl:basis-1/2 xl:p-0 px-5 sm:px-32 md:px-48 lg:px-80 py-28 text-n-very-light-gray text-center xl:text-start bg-mobile-intro-left-pattern bg-no-repeat sm:bg-none">
+        <hr className="hidden xl:block border-t-1 border-n-very-light-gray w-40" />
+        <h1 className="font-header text-5xl xl:text-7xl xl:mt-16">Humanitizing your insurance.</h1>
         <p className="mt-6 font-normal mb-8">
           Get your life insurance coverage easier and faster. We blend our
           expertise and technology to help you find the plan that's right for
